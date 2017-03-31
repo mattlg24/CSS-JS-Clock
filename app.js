97bdf32aa933d2018bd5d89681957d8ecc24119d
Matt Gardner <matthewlgardner@gmail.com>
2017-03-31 14:09:23 -0600

    add minute and hour functionality

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 window.onload = function() {
 
     let secondHand = document.querySelector('.second-hand')
+    let minuteHand = document.querySelector('.minute-hand')
+    let hourHand = document.querySelector('.hour-hand')
 
     function setDate() {
         let date = new Date()
+
         let second = date.getSeconds()
         let secondDegree = ((second / 60) * 360) + 90
         secondHand.style.transform = `rotate(${secondDegree}deg)`
-        console.log(second);
+        console.log(secondDegree);
+
+        let minute = date.getMinutes()
+        let minuteDegree = ((minute / 60) * 360) + 90
+        minuteHand.style.transform = `rotate(${minuteDegree}deg)`
+
+        let hour = date.getHours()
+        let hourDegree = ((hour / 12) * 360) + 90
+        hourHand.style.transform = `rotate(${hourDegree}deg)`
+
+        if (secondDegree === 444) {
+          secondHand.style.transition = null
+        }
     }
 
     setInterval(setDate, 1000)
